6474ebf934b4e648a209decb20c8729de649e33a
unknown <Carljohank@hotmail.com>
2024-10-07 10:08:04 +0200

    first commit

---
+++ src/main/java/Strings.java
@@ -0,0 +1,6 @@
+public class Strings {
+    String stringMetod(String question){
+        if(question.isEmpty()) return "IDK";
+        return "question";
+    }
+}
---
+++ src/test/java/StringsTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringsTest {
+
+    @Test
+    void stringMetod() {
+        Strings strings = new Strings();
+
+        String res = strings.stringMetod("e");
+        String res2 = strings.stringMetod("");
+
+        assertAll(
+                () -> assertEquals("question", res),
+                () -> assertEquals("IDK", res2),
+                () -> assertEquals("e", res2)
+        );
+    }
+}
